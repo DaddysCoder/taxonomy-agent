382a7452509c45f233b0e0f37c6d378ac4b15cdc
DaddysCoder <polina@stolyarova.com.au>
2026-09-24 20:11:01 +0800

    Import recovered taxonomy agent source snapshot

---
+++ taxonomy_agent/core/__init__.py
@@ -0,0 +1,6 @@
+from .taxonomy_loader import TaxonomyLoader, TaxonomyNode
+from .embedder import TaxonomyEmbedder
+from .file_reader import FileReader
+from .classifier import FileClassifier, ClassificationResult, ClassificationTier
+from .organiser import FileOrganiser, build_taxonomy_map
+from .watcher import FolderWatcher
